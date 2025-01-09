90f23f1c4cd7f97f092efd582bce686dddf8a125
Hehnev Alexey <manianahek@gmail.com>
2025-01-09 21:47:56 +0300

    init

---
+++ chat/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ru.hehnev.chat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ru.hehnev.chat to javafx.fxml;
+    exports ru.hehnev.chat;
+}
---
+++ chat/src/main/java/ru/hehnev/chat/HelloController.java
@@ -0,0 +1,14 @@
+package ru.hehnev.chat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
